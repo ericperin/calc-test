367082d8b0e0842b835703bc22d24b149747c318
Eric Perin <eric.perin@outlook.com>
2019-09-01 10:28:30 -0400

    Added JurosController

---
+++ src/Softplan.Calc.Api/Controllers/JurosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Softplan.Calc.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TaxaJurosController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<double> Get()
+        {
+            return 0.01;
+        }
+    }
+}
