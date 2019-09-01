52bec225af5b39fb659b8910228bc1b21c556f89
Eric Perin <eric.perin@outlook.com>
2019-09-01 12:32:31 -0400

    Added domain project

---
+++ src/Softplan.Calc.Api/Controllers/CalculaJurosController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Softplan.Calc.Domain.Entities;
+
+namespace Softplan.Calc.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CalculaJurosController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<decimal> Get(decimal valorInicial, int meses)
+        {
+            var taxaJurosController = new TaxaJurosController();
+            var valorJuros = taxaJurosController.Get();            
+
+            var juros = new Juros(valorJuros.Value);
+            return juros.Calcula(valorInicial, meses);
+        }
+    }
+}
---
+++ src/Softplan.Calc.Domain/Entities/Juros.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Softplan.Calc.Domain.Entities
+{
+    public class Juros
+    {
+        public Juros(double valor)
+        {
+            Valor = valor;
+        }
+        public double Valor { get; private set; }
+
+        public decimal Calcula(decimal valorInicial, int meses)
+        {
+            var potencia = Math.Pow(1 + Valor, meses);
+            var valorFinal = Convert.ToDecimal(Convert.ToDouble(valorInicial) * potencia);
+
+            return Decimal.Round(valorFinal, 2);
+        }
+    }
+}
