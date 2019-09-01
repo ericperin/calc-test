8f53436fde0315cf6d3d57126b39d52e7f940861
Eric Perin <eric.perin@outlook.com>
2019-09-01 12:43:39 -0400

    Added showmethecode link

---
+++ src/Softplan.Calc.Api/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Softplan.Calc.Api.Controllers
+{
+    [Route("showmethecode")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "https://github.com/ericperin/calc-test";
+        }
+    }
+}
